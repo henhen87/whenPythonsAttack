e7a68a04c22865f0aad4f476fb161d40c605b38a
henry <btnysci@yahoo.com>
2018-10-03 20:17:16 -0700

    ppp

--- intro.py
+++ intro.py
@@ -2,3 +2,21 @@ print("Hello, please type in your name:\n");
 name = input();
 
 print(f"The name you have entered is {name}");
+
+string = "Hello World";
+print(string);
+
+tupple = ('This', 'is', 'a', 'tupple', 1, 2, 3);
+
+print(tupple);
+
+lis = ['this', 'is', 'a', 'list', 1, 2, 3];
+print(lis);
+
+if len(lis) > 0:
+	print('list length is greater than 0');
+elif len(lis) < 0:
+	print('nothing in list')
+
+if len(tupple) > 0:
+	print('tupple length is greater than 0')

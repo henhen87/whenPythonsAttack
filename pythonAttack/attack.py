f8aef61150a2a310fb9f13a7a8ddaa2cd82db55f
henry <btnysci@yahoo.com>
2018-12-01 03:35:51 -0800

    :flower:

---
+++ pythonAttack/attack.py
@@ -0,0 +1,12 @@
+import webtest
+
+import pythonAttack
+
+
+def test_get():
+    app = webtest.TestApp(pythonAttack.app)
+
+    response = app.get('/')
+
+    assert response.status_int == 200
+    assert response.body == 'Hello Elena!!!!!!!'
--- pythonAttack/pythonAttack.py
+++ pythonAttack/pythonAttack.py
@@ -1,3 +1,11 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello Elena!!!!!!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)

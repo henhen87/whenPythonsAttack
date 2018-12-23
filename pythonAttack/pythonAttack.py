62b5234167c836bc078bc4ab6272c3bdea9bf273
henry <btnysci@yahoo.com>
2018-12-22 23:55:03 -0800

    uuu

--- pythonAttack/pythonAttack.py
+++ pythonAttack/pythonAttack.py
@@ -9,13 +9,17 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
+		print('FILE {}'.format(__file__))
+		print('PATH ABS {}'.format(os.path.abspath(__file__)))
+		VIEW = os.path.join(os.getcwd(), 'dist', 'index.html')
+		print('VIEEWWWWWWW {}'.format(VIEW))
 		url = 'python.gif'
 
 		template_values = {
 			'url': url
 		}
 
-		template = JINJA_ENVIRONMENT.get_template('index.html')
+		template = JINJA_ENVIRONMENT.get_template(VIEW)
 		self.response.write(template.render(template_values))
 
 

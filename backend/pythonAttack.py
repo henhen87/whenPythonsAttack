c9b1fdcc98d945a331cbf9a955243fc7f2550830
henry <btnysci@yahoo.com>
2018-12-25 13:25:36 -0800

    lv

--- backend/pythonAttack.py
+++ backend/pythonAttack.py
@@ -3,13 +3,13 @@ import jinja2
 import webapp2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-  loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(), 'views')),
+  loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(), 'backend', 'views')),
   extensions=['jinja2.ext.autoescape'],
   autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-		VIEW = os.path.join(os.getcwd(), 'dist', 'index.html')
+		VIEW = os.path.join(os.getcwd(), 'backend', 'views')
 		print('VIEEWWWWWWW {}'.format(VIEW))
 		url = 'python.gif'
 

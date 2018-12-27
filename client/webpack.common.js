cb2d85f82b999fbbf802a82a25fce5646a67071d
henry <btnysci@yahoo.com>
2018-12-26 18:26:43 -0800

    sassy

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,21 +1,25 @@
-var path      = require('path');
-const fs      = require('fs');
+var path      = require("path");
+const fs      = require("fs");
 
 const appRoot = fs.realpathSync(process.cwd());
-const appModules = path.resolve(appRoot, 'node_modules');
+const appModules = path.resolve(appRoot, "node_modules");
 const components = path.join(process.cwd(), "components");
 const containers = path.join(process.cwd(), "containers");
 const RMODS = [appModules, components, containers];
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const SASS_TYPE = require("sass");
+
+console.log('PATHH', path.join(process.cwd(), "../dist/css"))
 
 module.exports = {
-  entry: "./index.js",
+  entry: ["./index.js", "./sass/index.scss"],
   output: {
     path: path.join(process.cwd(), "../dist/js"),
     filename: "mybundle.js"
   },
   resolve: {
     modules: RMODS, 
-    extensions: ['.js'] 
+    extensions: [".js"] 
   },
   module: {
     rules: [
@@ -28,10 +32,29 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.join(process.cwd(), "../dist/css")
+            }
+          },
+          "css-loader",
+          "sass-loader",
+        ]
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../dist/css/index.css",
+      chunkFilename: "[name].css"
+    })
+  ],
   stats: {
     colors: true
   }
